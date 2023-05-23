870027bf46d765bfd730298bbb9edb96331ee137
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-23 15:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function callme(){
+	let element = document.createElement("div");
+	element.id = "browser-info";
+
+	element.innerText = `You are using ${navigator.appName} version ${appVersion}`;
+	document.body.append(element);
+
+}
+callme();
